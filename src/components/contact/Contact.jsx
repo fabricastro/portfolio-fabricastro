5d7191e91eee28f03149a524488db448475008c7
Osvaldo <95934997+oatencio@users.noreply.github.com>
2024-07-04 16:47:23 -0300

    arreglo de diseño

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -11,27 +11,27 @@ export const Contact = () => {
       <h5>Ponte en contacto</h5>
       <h2>Conmigo</h2>
       
-      <div className="container contact__container">
+      <div className="container contact__container w-[58%] md:w-[20%]">
         <div className="contact__options">
           <article className="contact__option">
             <MdOutlineEmail className='contact__option-icon' />
             <h4>Mail</h4>
             <h5>fabricastrook@gmail.com</h5>
-            <a href="mailto:fabricastrook@gmail.com" target={'_blank'}>Enviame un mensaje</a>
+            <a href="mailto:fabricastrook@gmail.com" target={'_blank'} className='text-[#00a76d]'>Enviame un mensaje</a>
           </article>
           
           <article className="contact__option">
             <BsLinkedin className='contact__option-icon' />
             <h4>LinkedIn</h4>
             <h5>Fabricio Castro</h5>
-            <a href="https://www.linkedin.com/in/fabricio-castrodev/" target={'_blank'}>Enviame un mensaje</a>
+            <a href="https://www.linkedin.com/in/fabricio-castrodev/" target={'_blank'} className='text-[#00a76d]'>Enviame un mensaje</a>
           </article>
           
           <article className="contact__option">
             <AiOutlineWhatsApp className='contact__option-icon' />
             <h4>Whatsapp</h4>
             <h5>+2644412511</h5>
-            <a href="https://wa.me/2644412511" target={'_blank'}>Enviame un mensaje</a>
+            <a href="https://wa.me/2644412511" target={'_blank'} className='text-[#00a76d]'>Enviame un mensaje</a>
           </article>
            
         </div>
